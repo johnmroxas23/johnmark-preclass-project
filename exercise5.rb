1d82a6f02f0485856363dfbe3ad9ec5bba761db8
johnmark <johnmark.roxas.23@gmail.com>
2022-05-31 16:02:01 +0800

    exercise5

--- exercise5.rb
+++ exercise5.rb
@@ -1,1 +1,16 @@
 
+
+
+def arrayAge(age)
+
+  ageArray  = Array.new
+
+  age.each do |number|
+      ageArray << number
+      return ageArray
+
+  end
+
+end
+
+print arrayAge([24,25,12,5,24,23,21])

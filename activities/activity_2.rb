82da2dad6068781f05a5ada8d4eb85c226849921
johnmark <johnmark.roxas.23@gmail.com>
2022-05-31 15:40:16 +0800

    activity 2

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+
+def name_hash(fullName)
+
+  fullName.each do |key, value|
+    print value
+  end
+
+end
+
+names = { first_name: "john" , middle_name:"morata", last_name:"roxas" }
+name_hash(names)
